851482a72057a435c6fd83648bf02c715ff1d23f
32602100045_Fatwaakbarjiwani <fatwaakbar2003@gmail.com>
2024-05-18 01:01:39 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+     port: 8080, // Ganti 8080 dengan port yang Anda inginkan
+   },
 })
